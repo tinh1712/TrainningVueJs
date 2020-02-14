1b773def599d3ae23e0c8025511dce92418ffbba
tinh1712 <tiing1712@gmail.com>
2020-02-14 11:02:28 +0700

    Update Cli

---
+++ Cli/cli/src/components/Child.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>{{textChild}}</h1>
+</template>
+
+<script>
+export default {
+    name: 'cli-child',
+    data() {
+        return {
+            textChild: 'child'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Cli/cli/src/components/Parent.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>{{textParent}}</h1>
+</template>
+
+<script>
+export default {
+    name: 'cli-parent',
+    data() {
+        return{
+            textParent: 'parent'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
