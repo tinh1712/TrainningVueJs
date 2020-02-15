cd98d5f9ae07ffa3f3e8ce357338ca2d0d538238
tinh1712 <tiing1712@gmail.com>
2020-02-15 09:39:09 +0700

    Update VueX

---
+++ VueX/demo-vuex/src/Components/Couter.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <button v-on:click="increment">Increment</button>
+        <button v-on:click="decrement">Decrement</button>   
+    </div>    
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+
+        }
+    },
+    methods: {
+        increment () {
+            this.$store.state.result++;
+        },
+        decrement () {
+            this.$store.state.result--;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ VueX/demo-vuex/src/Components/Result.vue
@@ -0,0 +1,17 @@
+<template>
+    <p>This is Result: {{ result }}</p>
+</template>
+
+<script>
+export default {
+    computed: {
+        result() {
+            return this.$store.state.result
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
